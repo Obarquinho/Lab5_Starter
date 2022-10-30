9a466670b658c64a4bc6e61ef22f53aa3bccfc7e
Obarquinho <pigsvanny@gmail.com>
2022-10-29 19:41:56 -0700

    part 1

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,29 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+  const voSelect = document.getElementById("voice-select");
+  let voices = [];
+
+  synth.addEventListener('voiceschanged', () => {
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length ; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voSelect.appendChild(option);
+    }
+    console.log(voices);
+  });
+  console.log(voices);
+
+  var timer = setInterval(function() {
+    voices = synth.getVoices();
+    console.log(voices);
+    if (voices.length !== 0) {
+      clearInterval(timer);
+    }
+}, 200);
 }
+
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,52 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSel = document.getElementById("horn-select");
+  var hornOpt = document.querySelectorAll("img")[0];
+  var audPath = "";
+  const audSel = document.getElementsByClassName("hidden");
+  const conf = new JSConfetti();
+  hornSel.addEventListener("change", function(){
+    if (this.value == "air-horn"){
+      hornOpt.src="assets/images/air-horn.svg";
+      audPath = "assets/audio/air-horn.mp3";
+      audSel[0].src = audPath;
+    }
+    else if (this.value == "car-horn"){
+      hornOpt.src = "assets/images/car-horn.svg";
+      audPath = "assets/audio/car-horn.mp3";
+      audSel[0].src = audPath;
+    }
+    else{
+      hornOpt.src = "assets/images/party-horn.svg";
+      audPath = "assets/audio/party-horn.mp3"
+      audSel[0].src = audPath;
+    }
+  });
+  const butSound = document.querySelectorAll("button")[0];
+  butSound.addEventListener("click", function(){
+    audSel[0].play();
+    if(audPath == "assets/audio/party-horn.mp3"){
+      conf.addConfetti();
+    }
+  });
+
+  const soundBar = document.getElementById("volume-controls");
+  const soundE = soundBar.querySelectorAll("input")[0];
+  const soundI = soundBar.querySelectorAll("img")[0];
+  soundE.addEventListener("change", function(){
+    audSel[0].volume = (soundE.value / 100.0);
+    if(soundE.value < 33 && soundE.value >= 1){
+      soundI.src = "assets/icons/volume-level-1.svg";
+    }
+    else if(soundE.value >= 33 && soundE.value < 67){
+      soundI.src = "assets/icons/volume-level-2.svg";
+    }
+    else if(soundE.value >= 67){
+      soundI.src = "assets/icons/volume-level-3.svg";
+    }
+    else{
+      soundI.src = "assets/icons/volume-level-0.svg";
+    }
+  })
 }
