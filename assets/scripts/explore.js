4c495ea8b83ae8e91a3895b788f826bad4fa692b
Ryan Vanny <73187503+Obarquinho@users.noreply.github.com>
2022-10-29 20:49:13 -0700

    Explore!

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -5,27 +5,53 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   const synth = window.speechSynthesis;
   const voSelect = document.getElementById("voice-select");
+  const but = document.querySelectorAll("button")[0];
+  const cont = document.getElementById("text-to-speak");
+  const smile = document.querySelectorAll("img")[0];
   let voices = [];
+  let utter = new SpeechSynthesisUtterance("your mom");
 
   synth.addEventListener('voiceschanged', () => {
     voices = synth.getVoices();
     for (let i = 0; i < voices.length ; i++) {
       const option = document.createElement('option');
       option.textContent = `${voices[i].name} (${voices[i].lang})`;
-      option.setAttribute('data-lang', voices[i].lang);
-      option.setAttribute('data-name', voices[i].name);
+      option.setAttribute('lang', voices[i].lang);
+      option.setAttribute('voice', voices[i].name);
       voSelect.appendChild(option);
     }
     console.log(voices);
   });
   console.log(voices);
 
-  var timer = setInterval(function() {
-    voices = synth.getVoices();
-    console.log(voices);
-    if (voices.length !== 0) {
-      clearInterval(timer);
+  voSelect.addEventListener("change", function(){
+    utter.lang = this.lang;
+    //utter.voice = this.name;
+    
+    console.log(this.options[1].value);
+    console.log(this.value);
+    for (let i = 0; i < this.options.length; i++){
+      if (this.options[i].value === this.value){
+        utter.voice = voices[i-1];
+        utter.lang = this.options[i].lang;
+        console.log(utter.voice);
+        console.log(utter.lang);
+      }
     }
-}, 200);
+  });
+
+  but.addEventListener("click", function(){
+    let te = cont.value;
+    utter.text = te;
+    synth.speak(utter);
+    if(synth.speaking){
+      smile.src = "assets/images/smiling-open.png";
+    }
+  })
+  var stod = setInterval(function() {
+    if(!synth.speaking){
+      smile.src = "assets/images/smiling.png";
+    }
+  },100)
 }
 
